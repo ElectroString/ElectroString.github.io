ea4f207d23833ddaa25cc4599ea4b7d5ad13327f
ElectroString <140640549+ElectroString@users.noreply.github.com>
2024-01-15 20:34:28 -0500

    Added More JS
    -JS Changed Image on click
    -Added Username function for custom greeting
    -Added Error Check to make sure the User inputted username is not blank

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,34 @@
-const myHeading = document.querySelector("h1");
-myHeading.textContent = "Hello world!";
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/Firefox-Logo.jpg") {
+    myImage.setAttribute("src", "images/firefox2.jpg");
+  } else {
+    myImage.setAttribute("src", "images/Firefox-Logo.jpg");
+  }
+};
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  if (!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Mozilla is cool, ${myName}`;
+  }
+}
+
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  const storedName = localStorage.getItem("name");
+  myHeading.textContent = `Mozilla is cool, ${storedName}`;
+}
+
+myButton.onclick = () => {
+  setUserName();
+};
